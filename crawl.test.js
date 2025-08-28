40d15fefba5cbbf6a2180ada5341bf900a00952d
Alexander <maina206mwangi@gmail.com>
2025-08-28 12:04:14 +0300

    update to crawl.js and our test code

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL", () => {
@@ -32,6 +32,22 @@ test("normalizeURL http", () => {
   const input = "http://blog.boot.dev/path/";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+
+  expect(actual).toEqual(expected);
+});
+
+//geturls fromhtml
+test("getURLsFromHTML", () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev">Boot.dev Blog</a>
+        </body>
+    </html>
+    `;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://blog.boot.dev/"];
 
   expect(actual).toEqual(expected);
 });
