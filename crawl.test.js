28d7274aa97b39cec3b2297e9e8039b235ffb111
Alexander <maina206mwangi@gmail.com>
2025-09-02 10:31:49 +0300

    Print SEO Report Crawler final version

--- crawl.test.js
+++ crawl.test.js
@@ -1,53 +1,69 @@
 const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeURL", () => {
-  const input = "https://blog.boot.dev/path/";
+test("normalizeURL protocol", () => {
+  const input = "https://blog.boot.dev/path";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
-
   expect(actual).toEqual(expected);
 });
 
-// trailing slash
-test("normalizeURL trailing slash", () => {
+test("normalizeURL slash", () => {
   const input = "https://blog.boot.dev/path/";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
-
   expect(actual).toEqual(expected);
 });
 
-// capitals
 test("normalizeURL capitals", () => {
-  const input = "https://BLOG.boot.dev/path/";
+  const input = "https://BLOG.boot.dev/path";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
-
   expect(actual).toEqual(expected);
 });
 
-// http protocol
 test("normalizeURL http", () => {
-  const input = "http://blog.boot.dev/path/";
+  const input = "http://BLOG.boot.dev/path";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
-
   expect(actual).toEqual(expected);
 });
 
-//geturls fromhtml
-test("getURLsFromHTML", () => {
-  const inputHTMLBody = `
-    <html>
-        <body>
-            <a href="https://blog.boot.dev">Boot.dev Blog</a>
-        </body>
-    </html>
-    `;
-  const inputBaseURL = "https://blog.boot.dev";
-  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+test("getURLsFromHTML absolute", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
   const expected = ["https://blog.boot.dev/"];
+  expect(actual).toEqual(expected);
+});
 
+test("getURLsFromHTML relative", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://blog.boot.dev/path/one"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [
+    "https://blog.boot.dev/path/one",
+    "https://other.com/path/one",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML handle error", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
